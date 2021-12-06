7a892339d0bb1ba6c7df902732b3ad4d5dcf4fbc
Jakub Gawęcki <jmgawecki@gmail.com>
2021-12-06 21:21:23 +0000

    FSS-19 Implement Animation Transition for FilmDetailScreen's Poster Image
    - Animation for the appearing poster within the FilmDetailsScreen has been implemented
    - Save to favourites button is now animated
    - FSBorderedButton has been expanded to optionally animate button onTapGesture
    
    #FSS-19

--- FilmStarSwiftUI/FilmStarSwiftUI/CustomView/Button/FSBorderedButton.swift
+++ FilmStarSwiftUI/FilmStarSwiftUI/CustomView/Button/FSBorderedButton.swift
@@ -5,14 +5,23 @@ struct FSBorederedButton: View {
     let systemImage: String
     let colour: Color
     let size: ControlSize
+    var isAnimated: Bool = false
     var accessibilityLabel: String? = nil
     var accessibilityHint: String? = nil
+    @State private var animationAngle: Double = 0
     
     let action: () -> Void
     
     var body: some View {
-        Button(action: action) {
+        Button(action: {
+            action()
+            if isAnimated {
+                animationAngle += 360
+            }
+        }) {
             Image(systemName: systemImage)
+                .rotationEffect(.degrees(animationAngle))
+                .animation(.easeIn, value: animationAngle)
             Text(title)
         }
         .buttonStyle(.bordered)
@@ -23,3 +32,4 @@ struct FSBorederedButton: View {
         .accessibilityHint(accessibilityHint ?? "")
     }
 }
+
